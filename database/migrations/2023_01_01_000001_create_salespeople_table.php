50c3b81b2836455df4ba828c145655896ed59a2c
axlmabugat <akujinuhara@gmail.com>
2025-05-01 23:57:32 +0800

    Update 2023_01_01_000001_create_salespeople_table.php

--- database/migrations/2023_01_01_000001_create_salespeople_table.php
+++ database/migrations/2023_01_01_000001_create_salespeople_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
 {
     Schema::create('salespeople', function (Blueprint $table) {
@@ -17,11 +14,7 @@ return new class extends Migration
         $table->timestamps();
     });
 }
-
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('salespeople');
